2518e6a28b5a5f95d51363ec1d03343d8963be87
barbarasampaioo <150027393+barbarasampaioo@users.noreply.github.com>
2024-04-05 08:05:38 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header('Jogando uma moeda')
+
+st.write('Ainda não é um aplicativo funcional. Em construção.')
+$ ls -a
+.git .gitignore .streamlit app.py README.md requirements.txt
+$ git add .
+$ git commit -am "initial commit"
+$ git push
+pip install streamlit
+streamlit run app.py
